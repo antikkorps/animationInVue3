8b31b431d7d68675c020467e98d3c8a9ddc856be
antikkorps <franckvienot7@gmail.com>
2023-06-05 14:19:00 +0200

    added this small project on animating transition in vue3

---
+++ src/views/AboutView.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas deleniti
+      cum saepe sapiente magni ex illo, accusantium, ducimus doloremque
+      voluptatum distinctio ut! Expedita dignissimos iste repellendus ex
+      repudiandae. Illo, suscipit? Delectus nobis incidunt deleniti consequuntur
+      earum, amet recusandae minima inventore aperiam eum dolorem rerum debitis
+      nostrum magnam odit et distinctio sequi ipsum quo nihil ipsa fuga! Itaque
+      voluptatibus minima harum. Cumque, hic suscipit aliquam totam earum dolor
+      mollitia eos corporis nam delectus alias provident deleniti expedita odio
+      corrupti ea. Impedit cumque perspiciatis cupiditate esse aut? Modi fugiat
+      odit fugit dicta. Corrupti praesentium, magnam numquam repellendus totam
+      voluptates nihil excepturi exercitationem hic architecto expedita
+      voluptatibus nemo perferendis iure consequuntur dolore voluptatum. Magnam
+      aspernatur sapiente, eos ipsam rem delectus sunt consequatur ducimus!
+      Atque itaque recusandae neque molestiae asperiores ea quo sit accusamus.
+      Aperiam minus necessitatibus voluptatibus illum iure ipsam atque numquam
+      inventore qui, ipsa laudantium labore beatae ab exercitationem? Deleniti,
+      nobis pariatur. Nulla quasi quia ipsa tempora optio nesciunt nihil
+      reprehenderit sint quis et. Voluptates optio nisi illo aut ab, porro
+      commodi culpa quam ipsam repellat aliquid non nulla numquam esse dolore!
+      Exercitationem cupiditate sed esse temporibus dolores ut ab quis iure
+      deleniti? Aliquam aspernatur odit reprehenderit, assumenda eum itaque
+      officia molestias, temporibus recusandae quam modi obcaecati incidunt quia
+      distinctio. Fuga, consequuntur. Eos modi a quisquam dolor numquam aperiam
+      doloribus obcaecati deleniti, vel unde molestiae quaerat, sapiente
+      eligendi quae, reiciendis quis vero hic excepturi. Incidunt fugiat impedit
+      ad dolore totam debitis ipsum. Sapiente provident consequuntur cumque,
+      nihil deserunt reiciendis, explicabo maiores saepe, autem quasi quia?
+      Beatae eum asperiores nisi culpa nesciunt quia quae distinctio sit, soluta
+      ad cupiditate, libero debitis provident cumque. Repellat cupiditate
+      corrupti nam, rem veniam corporis impedit sequi ad? Hic ipsa eaque dolorem
+      natus, explicabo corrupti a rerum veniam repudiandae quod blanditiis
+      consequuntur maxime nisi atque sapiente veritatis incidunt! Aut vitae
+      saepe quos dolor accusantium velit reprehenderit possimus delectus officia
+      omnis excepturi aperiam earum, ipsa labore cum ullam commodi distinctio.
+      Modi aliquid nulla expedita libero unde quod sed eum. Quia ipsum adipisci
+      odit reprehenderit cumque ex ipsam optio cupiditate, accusamus ea, nulla
+      nostrum quae. Suscipit qui cumque sed quos, id explicabo quae odit in aut,
+      iure officiis perspiciatis consectetur? Soluta labore fugiat delectus iure
+      quam provident cupiditate temporibus aut in nam, saepe illum
+      exercitationem nesciunt! Tempora, nihil a distinctio quaerat repellendus
+      qui ullam dolores facere possimus nobis quo quibusdam. Obcaecati natus
+      necessitatibus ipsum! Atque fugit numquam suscipit error sapiente
+      similique consequuntur quibusdam, nulla dolor ipsa soluta hic nisi ut
+      earum, ab deleniti commodi impedit perferendis quam maxime, illo
+      consequatur! Beatae labore itaque esse sunt repudiandae quas dolorem
+      voluptatum! Suscipit tenetur distinctio debitis reprehenderit esse vitae
+      eligendi, consectetur numquam labore quis eum sint saepe iure dolorem
+      laborum sit qui eius. Necessitatibus praesentium modi perferendis odio
+      dolore repellendus temporibus sunt nostrum rem enim provident non quos
+      impedit ab consequatur reiciendis sed animi, similique nemo fugit quam!
+      Repellat id vero perferendis impedit. Voluptatem unde ex laudantium
+      tempora!
+    </p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    max-width: 300px;
+    margin: 0 auto;
+  }
+}
+</style>
